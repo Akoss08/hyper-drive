575e389fdfbe2d952add51b44171162cc9bacbb2
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-19 14:39:22 +0100

    configure to redirect all booking/ requests to booking_url

--- backend/api/urls/__init__.py
+++ backend/api/urls/__init__.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("car/", include("api.urls.car_url")),
     path("track/", include("api.urls.track_url")),
+    path("booking/", include("api.urls.booking_url")),
 ]
