5c98748588cdffe6cbc3cc61c63a5085c1f3fc7e
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-19 14:38:53 +0100

    Create booking view and BookingListCreate class within

---
+++ backend/api/views/booking_view.py
@@ -0,0 +1,15 @@
+from ..models import Booking
+from rest_framework import generics
+from ..serializers.booking_serializer import BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class BookingListCreate(generics.ListCreateAPIView):
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
