5560b63f33328930f82687eccebb4f84ab3ad93e
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-19 11:33:33 +0100

    Create car model

--- backend/api/models/car.py
+++ backend/api/models/car.py
@@ -2,9 +2,18 @@ from django.db import models
 
 
 class Car(models.Model):
-    name = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
     model = models.CharField(max_length=50)
-    year = models.IntegerField()
+    year = models.PositiveIntegerField()
+    engine_size = models.DecimalField(max_digits=3, decimal_places=1)
+    horsepower = models.PositiveIntegerField()
+    cylinders = models.PositiveIntegerField()
+    torque = models.PositiveIntegerField()
+    top_speed = models.PositiveIntegerField()
+    kph_from_zero_to_hundred = models.DecimalField(
+        max_digits=4, decimal_places=1
+    )
+    model_asset_path = models.CharField()
 
     def __str__(self):
-        return f"{self.name} {self.model} ({self.year})"
+        return f"{self.make} {self.model} ({self.year})"
