d966143833086a68dd2361e3821644d86940a524
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-19 14:37:59 +0100

    Create booking model

--- backend/api/models/booking.py
+++ backend/api/models/booking.py
@@ -1,11 +1,37 @@
 from django.db import models
 from .user import CustomUser
+from .car import Car
+from .track import Track
+from django.core.exceptions import ValidationError
 
 
 class Booking(models.Model):
     user = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name="bookings"
     )
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=False)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, null=False)
+    booked_date = models.DateTimeField()
+
+    class Meta:
+        unique_together = ("user", "booked_date")
+
+    def clean(self):
+        booked_day = self.booked_date.date()
+
+        existing_bookings = Booking.objects.filter(
+            user=self.user, booked_date__date=booked_day
+        )
+
+        if existing_bookings.exists():
+            raise ValidationError(
+                "You cannot book multiple times on the same day."
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return "something"
+        return f"""{self.user.username} ({self.booked_date}):
+        {self.car.make}, {self.car.model} - {self.track.location}"""
