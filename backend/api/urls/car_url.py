eadc184e64924ca6ee997f53b04c1bf8cb3cb38b
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-20 12:34:34 +0100

    add url path to retrieve a car by id

--- backend/api/urls/car_url.py
+++ backend/api/urls/car_url.py
@@ -2,5 +2,6 @@ from django.urls import path
 from ..views import car_view
 
 urlpatterns = [
-    path("", car_view.CarListCreate.as_view(), name="car-list")
+    path("", car_view.CarListCreate.as_view(), name="car-lis-create"),
+    path("<int:pk>/", car_view.CarGetDetails.as_view(), name="car-detail"),
 ]
