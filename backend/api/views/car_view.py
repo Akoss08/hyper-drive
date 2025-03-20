e494564861f298b3a3093cdb60932b1e4c22e7bc
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-20 12:34:15 +0100

    Create CarGetDetails class to retrieve car by id

--- backend/api/views/car_view.py
+++ backend/api/views/car_view.py
@@ -1,7 +1,7 @@
 from ..models import Car
 from rest_framework import generics
 from ..serializers.car_serializer import CarSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.exceptions import PermissionDenied
 
 
@@ -14,3 +14,9 @@ class CarListCreate(generics.ListCreateAPIView):
         if not self.request.user.is_staff:
             raise PermissionDenied("You must be an admin to create a new car.")
         serializer.save()
+
+
+class CarGetDetails(generics.RetrieveAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [AllowAny]
