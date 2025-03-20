6d4f4ee4adee63e08261911f88bbc6f52da8388a
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-20 10:59:13 +0100

    add user handling to admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Car, Track, Booking
+from .models import Car, Track, Booking, CustomUser
 
 
 @admin.register(Car)
@@ -15,3 +15,8 @@ class TrackAdmin(admin.ModelAdmin):
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Booking._meta.fields]
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CustomUser._meta.fields]
