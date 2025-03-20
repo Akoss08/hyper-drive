1c0da31254510c76a1cb9cf0ee7b68ebe835e025
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-20 11:00:49 +0100

    create crud operations for user (get current, delete current, update current)

--- backend/api/views/user_view.py
+++ backend/api/views/user_view.py
@@ -8,3 +8,19 @@ class CreateUserView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
+
+
+class GetCurrentUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class DeleteCurrentUserView(generics.DestroyAPIView):
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
