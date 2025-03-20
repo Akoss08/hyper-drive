5d3c49e8acc665b79088badfb9fbafa9c5123efa
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-20 11:07:46 +0100

    add user/ endpoint to __init__ to forward user related requests to user_url

--- backend/api/urls/__init__.py
+++ backend/api/urls/__init__.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("car/", include("api.urls.car_url")),
     path("track/", include("api.urls.track_url")),
     path("booking/", include("api.urls.booking_url")),
+    path("user/", include("api.urls.user_url")),
 ]
