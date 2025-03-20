d2c7c2c328387c0352ec3ab58335ec3323546ec6
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-20 14:04:19 +0100

    rename user class names to follow conventions

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views.user_view import CreateUserView
+from api.views.user_view import UserCreateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/user/register/", UserCreateView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
