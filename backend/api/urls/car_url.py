d1e174c681cba1a7709c24e34f72ddb8512a1f21
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-24 16:05:30 +0100

    fixed underscores in url

--- backend/api/urls/car_url.py
+++ backend/api/urls/car_url.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ..views import car_view
 
 urlpatterns = [
-    path("", car_view.CarListCreate.as_view(), name="car-lis-create"),
-    path("<int:pk>/", car_view.CarGetDetails.as_view(), name="car-detail"),
+    path("", car_view.CarListCreate.as_view(), name="car_lis_create"),
+    path("<int:pk>/", car_view.CarGetDetails.as_view(), name="car_detail"),
 ]
--- backend/api/urls/user_url.py
+++ backend/api/urls/user_url.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path(
         "me/",
         user_view.CurrentUserGetUpdateView.as_view(),
-        name="user-get-update",
+        name="user_get_update",
     ),
     path(
         "me/delete/",
         user_view.CurrentUserDeleteView.as_view(),
-        name="delete-user",
+        name="delete_user",
     ),
 ]
