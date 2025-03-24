82ca3adc81a46734a50588108fe146ba32017d0b
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-24 13:47:55 +0100

    import packages for integration test

---
+++ backend/api/tests/integration/test_user_integration.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from api.models import CustomUser
+
+class UserIntegrationTest(TestCase)
