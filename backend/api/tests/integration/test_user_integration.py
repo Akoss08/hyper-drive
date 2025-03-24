6589203d11b947e5f5cc7aaa6c94c37dad5618aa
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-24 16:09:21 +0100

    Test update data of logged in user

--- backend/api/tests/integration/test_user_integration.py
+++ backend/api/tests/integration/test_user_integration.py
@@ -124,3 +124,25 @@ class UserIntegrationTest(TestCase):
         self.assertEqual(
             profile_response.status_code, status.HTTP_401_UNAUTHORIZED
         )
+    def test_update_login_user(self):
+        login_url = reverse("get_token")
+        profile_url = reverse("user_get_update")
+
+        login_response = self.client.post(
+            login_url,
+            {
+                "username": self.user.username,
+                "password": self.user_data["password"],
+            },
+            format="json",
+        )
+
+        profile_response = self.client.patch(
+            profile_url,
+            {"username": "changed"},
+            HTTP_AUTHORIZATION=f"Bearer {login_response.data["access"]}",
+        )
+
+        self.assertEqual(profile_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(profile_response.data["username"], "changed")
+
