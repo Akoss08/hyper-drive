8312ecdd52ac89ac959c6ff3979f5c46541e482c
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-24 16:07:37 +0100

    Test login user

--- backend/api/tests/integration/test_user_integration.py
+++ backend/api/tests/integration/test_user_integration.py
@@ -68,3 +68,17 @@ class UserIntegrationTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_login_user(self):
+        url = reverse("get_token")
+        response = self.client.post(
+            url,
+            {
+                "username": self.user.username,
+                "password": self.user_data["password"],
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("ey", response.data["access"])
+
