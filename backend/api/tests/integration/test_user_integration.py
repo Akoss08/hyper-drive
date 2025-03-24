f79b3bc5a3425a694f51e1524fbc8942924569e3
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-24 15:05:22 +0100

    create setup for tests

--- backend/api/tests/integration/test_user_integration.py
+++ backend/api/tests/integration/test_user_integration.py
@@ -4,4 +4,14 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from api.models import CustomUser
 
-class UserIntegrationTest(TestCase)
+    def setUp(self):
+        self.client = APIClient()
+        self.user_data = {
+            "username": "testuser",
+            "first_name": "Test",
+            "last_name": "User",
+            "email": "testuser@gmail.com",
+            "password": "pass",
+        }
+        self.user = CustomUser.objects.create_user(**self.user_data)
+
