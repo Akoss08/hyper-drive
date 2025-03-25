74e40a6180f4d54732602d8b99908ff24e036ed7
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-25 10:00:46 +0100

    import packages for tests

---
+++ backend/api/tests/integration/test_car_integration.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from api.models import CustomUser, Car
