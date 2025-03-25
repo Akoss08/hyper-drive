968df05a99e7bcd90d4fb39f89a45e2a398019e9
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-25 10:23:09 +0100

    Implement setUp method by creating cars, users

--- backend/api/tests/integration/test_car_integration.py
+++ backend/api/tests/integration/test_car_integration.py
@@ -8,3 +8,44 @@ from api.models import CustomUser, Car
 class CarIntegrationTest(TestCase):
 
     def setUp(self):
+        self.client = APIClient()
+        self.user_data = {
+            "username": "testuser",
+            "first_name": "Test",
+            "last_name": "User",
+            "email": "testuser@gmail.com",
+            "password": "pass",
+        }
+        self.admin_data = {
+            "username": "admin",
+            "first_name": "Ad",
+            "last_name": "Min",
+            "email": "admin@gmail.com",
+            "password": "pass",
+        }
+        self.user = CustomUser.objects.create_user(**self.user_data)
+        self.admin = CustomUser.objects.create_superuser(**self.admin_data)
+        self.car_1 = Car.objects.create(
+            make="Ford",
+            model="GT",
+            year=2024,
+            engine_size=3.5,
+            horsepower=650,
+            cylinders=12,
+            torque=500,
+            top_speed=380,
+            kph_from_zero_to_hundred=2.5,
+            model_asset_path="some/path/to/model",
+        )
+        self.car_2 = Car.objects.create(
+            make="Porsche",
+            model="GT 3 RS",
+            year=2024,
+            engine_size=3.5,
+            horsepower=650,
+            cylinders=12,
+            torque=500,
+            top_speed=380,
+            kph_from_zero_to_hundred=2.5,
+            model_asset_path="some/path/to/model",
+        )
