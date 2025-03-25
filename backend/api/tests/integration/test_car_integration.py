43857a091e588260b8a09d372e2e970e136abcae
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-25 10:22:16 +0100

    Create CarIntegrationTest class

--- backend/api/tests/integration/test_car_integration.py
+++ backend/api/tests/integration/test_car_integration.py
@@ -3,3 +3,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from api.models import CustomUser, Car
+
+
+class CarIntegrationTest(TestCase):
+
+    def setUp(self):
