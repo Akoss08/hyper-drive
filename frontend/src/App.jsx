97757505019a6661a8acfed3721d9ebc80f578b1
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-27 10:57:06 +0100

    Implement Logout function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,22 @@
-function App() {
+import { createBrowserRouter, Outlet, RouterProvider, Navigate } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Home from './pages/Home';
+import Cars from './pages/Cars';
+import Car from './pages/Car';
+import Circuits from './pages/Circuits';
+import Circuit from './pages/Circuit';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+import Account from './pages/Account';
+import Booking from './pages/Booking';
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/" />;
+}
   return (
     <>
       <h1 className="text-red-500 bg-amber-200">Hali</h1>
