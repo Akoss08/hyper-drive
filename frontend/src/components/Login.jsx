3bc534f9d7e07dfaf35d0ee4d228f3fc65fb3380
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-03-30 20:21:23 +0200

    Create states for username, password, loading in Login component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,16 @@
-function Login() {
-  return <div>Login</div>;
+import { useState } from 'react';
+import { Button, Dialog, Card, CardBody, CardFooter, Typography, Input, Checkbox } from '@material-tailwind/react';
+import api from '../api';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
+import { useNavigate } from 'react-router-dom';
+
+function Login({ onToggle, isOpen }) {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
 }
 
 export default Login;
