8efbd728d17c8a2fd3d2c508fc9adda12c356ab0
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-02 11:47:35 +0200

    Create rotating & moving & opacity transformations

--- frontend/src/components/HorizontalCarScrollSection.jsx
+++ frontend/src/components/HorizontalCarScrollSection.jsx
@@ -10,3 +10,7 @@ const HorizontalCarScrollSection = () => {
     target: targetRef,
     offset: ["start start", "end start"],
   });
+
+  const x = useTransform(scrollYProgress, [0, 1], ['-5%', '190%']);
+  const rotate = useTransform(scrollYProgress, [0, 1], ['0deg', '1500deg']);
+  const opacity = useTransform(scrollYProgress, [0.6, 1], [1, 0]);
