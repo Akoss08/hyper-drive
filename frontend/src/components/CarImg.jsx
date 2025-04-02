fe4c01647c0fcd62d2fc78f1e2477cd049158fd6
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-02 11:44:42 +0200

    Create CarImg component

---
+++ frontend/src/components/CarImg.jsx
@@ -0,0 +1,11 @@
+import carImg from '../assets/cars/pexels-pixabay-261986-removebg-preview.png';
+
+const CarImg = () => {
+  return (
+    <div className="h-[380px] w-[650px]">
+      <img src={carImg} />
+    </div>
+  );
+};
+
+export default CarImg;
