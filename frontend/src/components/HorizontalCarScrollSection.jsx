0ba4de127e1b73e312bb6e6d5f030cef6b057b24
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-02 11:48:15 +0200

    Build html elements & components for the section

--- frontend/src/components/HorizontalCarScrollSection.jsx
+++ frontend/src/components/HorizontalCarScrollSection.jsx
@@ -14,3 +14,21 @@ const HorizontalCarScrollSection = () => {
   const x = useTransform(scrollYProgress, [0, 1], ['-5%', '190%']);
   const rotate = useTransform(scrollYProgress, [0, 1], ['0deg', '1500deg']);
   const opacity = useTransform(scrollYProgress, [0.6, 1], [1, 0]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh]">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ opacity }}>
+          <RoadImg />
+        </motion.div>
+        <motion.div style={{ x, opacity }}>
+          <CarImg />
+          <WheelImg rotate={rotate} startingPosition={'bottom-[34.8%] left-[16.9%]'} />
+          <WheelImg rotate={rotate} startingPosition={'bottom-[35.1%] right-[20.7%]'} />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HorizontalCarScrollSection;
