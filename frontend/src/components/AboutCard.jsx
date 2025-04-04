28c2b959ca662271549fa2845fd127a53846d3ce
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-04 13:49:53 +0200

    Implement handleMouseMove function

--- frontend/src/components/AboutCard.jsx
+++ frontend/src/components/AboutCard.jsx
@@ -17,3 +17,21 @@ const HALF_ROTATION_RANGE = 32.5 / 2;
   const ySpring = useSpring(y);
 
   const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
+
+  const handleMouseMove = (e) => {
+    if (!ref.current) return [0, 0];
+
+    const rect = ref.current.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+    const rY = mouseX / width - HALF_ROTATION_RANGE;
+
+    x.set(rX);
+    y.set(rY);
+  };
