5440ae3d36d6ab6a5f126c213d91287ff331fa4d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-17 18:04:14 +0200

    Add CarScene to cars component

--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,5 +1,17 @@
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import CarScene from "../components/CarScene";
+
 function Cars() {
-  return <div>Cars</div>;
+  return (
+    <div className="h-screen">
+      <Canvas shadows>
+        <Suspense fallback={null}>
+          <CarScene />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default Cars;
