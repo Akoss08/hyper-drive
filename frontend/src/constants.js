c5edaaef01562dfb2ad4299002f73c9e0a863c75
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-05-01 11:13:27 +0200

    Add model settings to 3d models

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,29 @@
 export const ACCESS_TOKEN = 'access';
 export const REFRESH_TOKEN = 'refresh';
+
+export const MODEL_SETTINGS = {
+  'porsche-911-gt3-rs': {
+    scale: 0.5,
+    yOffset: 1.3,
+  },
+  'bugatti-bolide': {
+    scale: 2.1,
+    yOffset: 1.1,
+  },
+  'bentley-continental-gt3': {
+    scale: 175,
+    yOffset: 1.4,
+  },
+  'mercedes-benz-amg-gt': {
+    scale: 1.5,
+    yOffset: 1.2,
+  },
+  'ferrari-sf-90-xx': {
+    scale: 200,
+    yOffset: 1.2,
+  },
+  'aston-martin-valkyrie': {
+    scale: 2.25,
+    yOffset: 1.15,
+  },
+};
