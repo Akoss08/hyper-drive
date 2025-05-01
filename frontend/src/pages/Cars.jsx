c684e694b33d460bb1b3e65a34b7b61e732ff94d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-05-01 11:25:15 +0200

    Add initial states for cars page that tracks cars, index, nextUrl, prevUrl

--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,8 +1,19 @@
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
-import CarScene from "../components/CarScene";
+import { Suspense, useEffect, useState, useCallback } from 'react';
+import api from '../api';
+import { useGLTF } from '@react-three/drei';
+import CameraAndLight from '../components/CameraAndLight';
+import CarModel from '../components/CarModel';
+import LoadingSpinner from '../components/LoadingSpinner';
+import CarDetails from '../components/CarDetails';
+import NavigationButtons from '../components/NavigationButtons';
 
 function Cars() {
+  const [cars, setCars] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [nextUrl, setNextUrl] = useState(null);
+  const [prevUrl, setPrevtUrl] = useState(null);
+
   return (
     <div className="h-screen">
       <Canvas shadows>
