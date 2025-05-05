6a5faf1916bb90ae990dde288fc6e16296b756dd
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-05-05 13:03:15 +0200

    Add model settings for circuits

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -26,4 +26,34 @@ export const MODEL_SETTINGS = {
     scale: 2.25,
     yOffset: 1.15,
   },
+  monaco: {
+    scale: 0.3,
+    yOffset: 1,
+  },
+  hungaroring: {
+    scale: 0.5,
+    yOffset: 1,
+  },
+  belgium: {
+    scale: 0.4,
+    yOffset: 1,
+  },
+  canada: {
+    scale: 0.5,
+    yOffset: 1,
+  },
+  monza: {
+    scale: 0.8,
+    yOffset: 1,
+  },
+  silverstone: {
+    scale: 0.4,
+    yOffset: 1,
+  },
+};
+
+export const difficultyColors = {
+  Easy: '#00cc44',
+  Medium: '#ffb366',
+  Hard: '#ff3333',
 };
